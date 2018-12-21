d074d624efe967d0704942ad606c6d6cc9a9a35c
TheIcyOne <icy.headfishindustries@gmail.com>
2018-12-21 15:16:54 +0000

    I'm not sure what happened here.

--- src/main/java/com/headfishindustries/hydrophobia/EventHandler.java
+++ src/main/java/com/headfishindustries/hydrophobia/EventHandler.java
@@ -9,7 +9,9 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.SERVER)
 public final class EventHandler {
 	
-	double rainChance = ConfigHandler.rainCancelChance;
+	static double rainChance = ConfigHandler.rainCancelChance;
+	static double thunderChance = ConfigHandler.thunderCancelChance;
+	static int thunderMode = ConfigHandler.thunderMode;
 	
 	public static void registerEvents(){
 		EventHandler events = new EventHandler();
@@ -17,7 +19,7 @@ public final class EventHandler {
 	}
 	
 	@SubscribeEvent
-	public void onTick(WorldTickEvent e){
+	public static void onTick(WorldTickEvent e){
 		if (e.world.getWorldInfo().getRainTime() == 1000){
 			if (e.world.rand.nextDouble() <= rainChance/100){
 				e.world.getWorldInfo().setRainTime(0);
@@ -25,5 +27,26 @@ public final class EventHandler {
 				Hydrophobia.LOGGER.info("The rain is staved off.");
 			}
 		}
+		
+		if (e.world.getWorldInfo().getThunderTime() == 1000) {
+			if (e.world.rand.nextDouble() <= thunderChance/100){
+				e.world.getWorldInfo().setThunderTime(0);
+				e.world.getWorldInfo().setThundering(false);
+				Hydrophobia.LOGGER.info("The storm is staved off.");
+			}
+		}
+		if (e.world.isThundering() && thunderMode > 0) {
+			if (thunderMode == 1) {
+                e.world.getWorldInfo().setThunderTime(0);
+                e.world.getWorldInfo().setThundering(false);
+                e.world.getWorldInfo().setRaining(true);
+			}else {
+				e.world.getWorldInfo().setRainTime(0);
+				e.world.getWorldInfo().setThunderTime(0);
+				e.world.getWorldInfo().setRaining(false);
+                e.world.getWorldInfo().setThundering(false);
+			}
+		}
 	}
+
 }
